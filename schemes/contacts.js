dd20ae726560d94ec6c71bf90fcee6d6582f218b
Maksym Dzhafarov <maksim8797@gmail.com>
2021-11-28 14:33:23 +0200

    update contactScheme

--- schemes/contacts.js
+++ schemes/contacts.js
@@ -1,8 +1,10 @@
 const Joi = require("joi");
 
 contactScheme = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+    .required(),
   phone: Joi.string().required(),
 });
 
